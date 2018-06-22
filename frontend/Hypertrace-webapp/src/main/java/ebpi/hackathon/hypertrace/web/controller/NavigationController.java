de5790eb2fc43f58527923481acafb89aff6cac7
kkerkhoven-ebpi <kevin.kerkhoven@ebpi.nl>
2018-06-22 20:31:37 +0200

    Maak cookie & stuff

--- frontend/Hypertrace-webapp/src/main/java/ebpi/hackathon/hypertrace/web/controller/NavigationController.java
+++ frontend/Hypertrace-webapp/src/main/java/ebpi/hackathon/hypertrace/web/controller/NavigationController.java
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @Controller
 public class NavigationController {
+
     /**
      * Homepage for non logged-in user
      * @param model
@@ -43,10 +46,16 @@ public class NavigationController {
      * @return logged in home or same page
      */
     @PostMapping("/login")
-    public String loginSubmit(@ModelAttribute(value="user") User user, @RequestParam("type") String type) {
+    public String loginSubmit(@ModelAttribute(value="user") User user, @RequestParam("type") String type, Map<String, Object> model, HttpServletResponse response) {
+        // clear cookies
         user.setType(type);
         System.out.println(new Gson().toJson(user));
-        return "home";
+
+        response.addCookie(new Cookie("username", user.getUsername()));
+        response.addCookie(new Cookie("type", user.getType()));
+
+
+        return loggedIn(model, user);
     }
 
     /**
@@ -55,9 +64,8 @@ public class NavigationController {
      * @return homepage logged-in user
      */
     @RequestMapping("/loggedIn")
-    public String loggedIn(Map<String, Object> model) {
-        String loggedInMessage = "{{username}}";
-        model.put("loggedInMessage", loggedInMessage);
+    public String loggedIn(Map<String, Object> model, User user) {
+        model.put("loggedInMessage", user.getType() + " " + user.getUsername() + "!");
         return "homeLoggedIn";
     }
 
