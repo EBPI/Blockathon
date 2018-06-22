4b82d81f7ed97c3d5f65502dfa9ecb3f0e8834a3
Rogier Morsink <rogier.morsink@ebpi.nl>
2018-06-22 18:53:10 +0200

    eerste poging logic

--- blockchain/lib/logic.js
+++ blockchain/lib/logic.js
@@ -1,8 +1,38 @@
-'use strict';
-/**
- * Write your transction processor functions here
- */
 
 {
 
+
+    /**
+     *  Placer Order
+     * @param {org.ebpi.blockathon.Order} PlaceOrder
+     * @transaction
+     */
+
+
+    function sendShipment(shipment){
+        const manufacturer= getCurrentParticipant()
+        const manuID = manufacturer.PartnerID
+        const hash = shipment.DocumentHash
+        const docLoc = shipment.DocumentLocation
+        const procductList = shipment.ShippedProducts
+        const transprterList = shipment.transporterList
+        const destination= shipment.Destination
+        const customer = shipment.Customer
+        const costID = costumer.PartnerID
+        const originalWeight = shipment.Weight
+        let factory = getFactory()
+        let newShipment = factory.newResource('org.ebpi.blockathon', 'Shipment')
+        newshipment.DocumentHash = hash
+        newshipment.DocumentLocation =docLoc
+        newshipment.ShippedProducts=procductList
+        newshipment.transporterList=transprterList
+        newshipment.Destination=destination
+        newshipment.Customer = factory.newRelationship('org.ebpi.blockathon', 'Orderer', costID)
+        newshipment.Supplier =factory.newRelationship('org.ebpi.blockathon', 'Manufacturer', manuID)
+        newshipment.Weight= originalWeight
+        return getAssetRegistry('org.ebpi.blockathon.Shipment')
+            .then(function (shipmentRegistry) {
+                return shipmentRegistry.add(newshipment)
+            })
+    }
 }
