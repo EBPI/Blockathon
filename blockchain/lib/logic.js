806809202258c1b42dfbd71795748771f7b6a357
Rogier Morsink <rogier.morsink@ebpi.nl>
2018-06-22 20:36:29 +0200

    id generator voor shipment

--- blockchain/lib/logic.js
+++ blockchain/lib/logic.js
@@ -27,9 +27,19 @@ function doSendShipment(sendShipment){
     newShipment.Customer = factory.newRelationship('org.ebpi.blockathon', 'Orderer', costID);
     newShipment.Supplier =factory.newRelationship('org.ebpi.blockathon', 'Manufacturer', manuID);
     newShipment.Weight= originalWeight;
+    newShipment.ShipmentID= uuidv4;
+      
     return getAssetRegistry('org.ebpi.blockathon.Shipment')
         .then(function (shipmentRegistry) {
             return shipmentRegistry.add(newShipment)
         })
-
+        
     }
+
+    function uuidv4() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+        });
+      }
+    
