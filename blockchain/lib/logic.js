a3e8e4e8eb2cd1984d6571b13295222041d5d947
Rogier Morsink <rogier.morsink@ebpi.nl>
2018-06-22 20:24:41 +0200

    shipment naar sendShipment

--- blockchain/lib/logic.js
+++ blockchain/lib/logic.js
@@ -7,26 +7,26 @@
 
 
 function doSendShipment(sendShipment){
-    const manufacturer= getCurrentParticipant()
-    const manuID = manufacturer.PartnerID
-    const hash = shipment.DocumentHash
-    const docLoc = shipment.DocumentLocation
-    const procductList = shipment.ShippedProducts
-    const transprterList = shipment.transporterList
-    const destination= shipment.Destination
-    const costumer = shipment.Customer
-    const costID = costumer.PartnerID
-    const originalWeight = shipment.Weight
-    let factory = getFactory()
-    let newShipment = factory.newResource('org.ebpi.blockathon', 'Shipment')
-    newShipment.DocumentHash = hash
-    newShipment.DocumentLocation =docLoc
-    newShipment.ShippedProducts=procductList
-    newShipment.transporterList=transprterList
-    newShipment.Destination=destination
-    newShipment.Customer = factory.newRelationship('org.ebpi.blockathon', 'Orderer', costID)
-    newShipment.Supplier =factory.newRelationship('org.ebpi.blockathon', 'Manufacturer', manuID)
-    newShipment.Weight= originalWeight
+    const manufacturer= getCurrentParticipant();
+    const manuID = manufacturer.PartnerID;
+    const hash = sendShipment.DocumentHash;
+    const docLoc = sendShipment.DocumentLocation;
+    const procductList = sendShipment.ShippedProducts;
+    const transprterList = sendShipment.transporterList;
+    const destination= sendShipment.Destination;
+    const costumer = sendShipment.Customer;
+    const costID = costumer.PartnerID;
+    const originalWeight = sendShipment.Weight;
+    let factory = getFactory();
+    let newShipment = factory.newResource('org.ebpi.blockathon', 'Shipment');
+    newShipment.DocumentHash = hash;
+    newShipment.DocumentLocation =docLoc;
+    newShipment.ShippedProducts=procductList;
+    newShipment.transporterList=transprterList;
+    newShipment.Destination=destination;
+    newShipment.Customer = factory.newRelationship('org.ebpi.blockathon', 'Orderer', costID);
+    newShipment.Supplier =factory.newRelationship('org.ebpi.blockathon', 'Manufacturer', manuID);
+    newShipment.Weight= originalWeight;
     return getAssetRegistry('org.ebpi.blockathon.Shipment')
         .then(function (shipmentRegistry) {
             return shipmentRegistry.add(newShipment)
