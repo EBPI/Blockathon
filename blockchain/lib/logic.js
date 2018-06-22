5ee92b1732f9aadd7509d338d0b835a7311c03f9
John Renne <john@Johns-MacBook-Pro.local>
2018-06-22 20:20:17 +0200

    Removed brackets from functions

--- blockchain/lib/logic.js
+++ blockchain/lib/logic.js
@@ -1,38 +1,35 @@
 
-{
+/**
+* Sample transaction
+* @param {org.ebpi.blockathon.sendShipment} sendShipment
+* @transaction
+*/
 
 
-    /**
-     * Sample transaction
-     * @param {org.ebpi.blockathon.sendShipment} sendShipment
-     * @transaction
-     */
+function doSendShipment(sendShipment){
+    const manufacturer= getCurrentParticipant()
+    const manuID = manufacturer.PartnerID
+    const hash = shipment.DocumentHash
+    const docLoc = shipment.DocumentLocation
+    const procductList = shipment.ShippedProducts
+    const transprterList = shipment.transporterList
+    const destination= shipment.Destination
+    const costumer = shipment.Customer
+    const costID = costumer.PartnerID
+    const originalWeight = shipment.Weight
+    let factory = getFactory()
+    let newShipment = factory.newResource('org.ebpi.blockathon', 'Shipment')
+    newShipment.DocumentHash = hash
+    newShipment.DocumentLocation =docLoc
+    newShipment.ShippedProducts=procductList
+    newShipment.transporterList=transprterList
+    newShipment.Destination=destination
+    newShipment.Customer = factory.newRelationship('org.ebpi.blockathon', 'Orderer', costID)
+    newShipment.Supplier =factory.newRelationship('org.ebpi.blockathon', 'Manufacturer', manuID)
+    newShipment.Weight= originalWeight
+    return getAssetRegistry('org.ebpi.blockathon.Shipment')
+        .then(function (shipmentRegistry) {
+            return shipmentRegistry.add(newShipment)
+        })
 
-
-    function doSendShipment(sendShipment){
-        const manufacturer= getCurrentParticipant()
-        const manuID = manufacturer.PartnerID
-        const hash = shipment.DocumentHash
-        const docLoc = shipment.DocumentLocation
-        const procductList = shipment.ShippedProducts
-        const transprterList = shipment.transporterList
-        const destination= shipment.Destination
-        const costumer = shipment.Customer
-        const costID = costumer.PartnerID
-        const originalWeight = shipment.Weight
-        let factory = getFactory()
-        let newShipment = factory.newResource('org.ebpi.blockathon', 'Shipment')
-        newShipment.DocumentHash = hash
-        newShipment.DocumentLocation =docLoc
-        newShipment.ShippedProducts=procductList
-        newShipment.transporterList=transprterList
-        newShipment.Destination=destination
-        newShipment.Customer = factory.newRelationship('org.ebpi.blockathon', 'Orderer', costID)
-        newShipment.Supplier =factory.newRelationship('org.ebpi.blockathon', 'Manufacturer', manuID)
-        newShipment.Weight= originalWeight
-        return getAssetRegistry('org.ebpi.blockathon.Shipment')
-            .then(function (shipmentRegistry) {
-                return shipmentRegistry.add(newShipment)
-            })
     }
-}
