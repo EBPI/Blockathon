26272a430f9c7c2a47a7573d63307401b11653fc
kkerkhoven-ebpi <kevin.kerkhoven@ebpi.nl>
2018-06-23 20:37:43 +0200

    Alvast wat DTO'tjes

---
+++ frontend/Hypertrace-webapp/src/main/java/ebpi/hackathon/hypertrace/web/domein/TradePartner.java
@@ -0,0 +1,14 @@
+package ebpi.hackathon.hypertrace.web.domein;
+
+public class TradePartner {
+    private String PartnerId;
+
+
+    public String getPartnerId() {
+        return PartnerId;
+    }
+
+    public void setPartnerId(String partnerId) {
+        PartnerId = partnerId;
+    }
+}
