99c1f5c489407da93702c98ee6268bddc173ddfc
Rogier Morsink <rogier.morsink@ebpi.nl>
2018-06-23 23:13:21 +0200

    simple testje gemaakt vor order.

---
+++ frontend/Hypertrace-webapp/src/test/java/ebpi/hackathon/hypertrace/web/domein/OrderTest.java
@@ -0,0 +1,29 @@
+package ebpi.hackathon.hypertrace.web.domein;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderTest {
+
+    @Test
+    public void testGettersAndSetters(){
+        Order order = new Order();
+        order.setManufacturer("blee");
+        order.setOrderer("eurderer");
+        List<String> ding = new ArrayList<>();
+        ding.add("hoi");
+        ding.add("tot Ziens");
+        order.setProducts(ding);
+        assert(order.getProducts().get(0).equals("hoi"));
+        assert(order.getProducts().get(1).equals("tot Ziens"));
+        assert(order.getManufacturer().equals("blee"));
+        assert(order.getOrderer().equals(("eurdere")));
+
+
+
+
+    }
+
+}
