c06380dfeb8b76020ba5e35b4fbd0b1ebed12873
kkerkhoven-ebpi <kevin.kerkhoven@ebpi.nl>
2018-06-24 02:13:09 +0200

    imports

--- frontend/Hypertrace-webapp/src/main/java/ebpi/hackathon/hypertrace/web/controller/OrderController.java
+++ frontend/Hypertrace-webapp/src/main/java/ebpi/hackathon/hypertrace/web/controller/OrderController.java
@@ -6,7 +6,6 @@ import ebpi.hackathon.hypertrace.web.rest.BackendService;
 import ebpi.hackathon.hypertrace.web.rest.HyperledgerRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
